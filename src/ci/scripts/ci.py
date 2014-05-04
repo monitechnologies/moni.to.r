b700b21426cdc81cdf1b3e25821d7167bd211b75
Russell Clare <Russell@Clare.io>
2014-05-04 01:10:53 +0100

    Changing from shell script to python

---
+++ src/ci/scripts/ci.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import argparse
+
+parser = argparse.ArgumentParser(description="This program process arguments")
+parser.add_argument("-C", "--commit", dest="commit",
+               help="Commit to checkout in task sequence", default="develop")
+parser.add_argument("-B", "--base",  dest="base",
+               help="Base to use for merge function", default="develop")
+parser.add_argument("-H", "--head", dest="head",
+               help="Head to use when merging in")
+
+args = parser.parse_args()
